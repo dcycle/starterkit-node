5a81e42caaa86ec39d88bf878b299e23c93de9fe
Albert Albala <albert@mediatribe.net>
2022-02-12 17:34:08 -0500

    Please squash this commit in the pull request

--- scripts/unit-tests-node.sh
+++ scripts/unit-tests-node.sh
@@ -7,6 +7,6 @@ source ./scripts/lib/start.source.sh
 
 echo "$MYINDENT"'=> Running unit tests on node scripts.'
 "$BIN"docker build -t my/starterkit-node .
-"$BIN"docker-compose -f docker-compose.yml -f docker-compose.dev.yml run node /bin/bash -c 'npm test'
+"$BIN"docker-compose exec node /bin/bash -c 'npm test'
 
 source ./scripts/lib/start.source.sh
