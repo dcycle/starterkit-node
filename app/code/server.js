72fce8b724a7fe72c2373e3ff17ada28d24c0057
Albert Albala <albert@mediatribe.net>
2022-02-26 08:53:50 -0500

    Please squash this commit in the pull request

--- app/code/server.js
+++ app/code/server.js
@@ -77,6 +77,50 @@
     console.log('***');
   });
 
+  const connectEnsureLogin = require('connect-ensure-login');
+
+  app.post('/login', (req, res, next) => {
+    passport.authenticate('local',
+    (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (!user) {
+        return res.redirect('/login?info=' + info);
+      }
+
+      req.logIn(user, function(err) {
+        if (err) {
+          return next(err);
+        }
+
+        return res.redirect('/');
+      });
+
+    })(req, res, next);
+  });
+
+  app.get('/login',
+    (req, res) => res.sendFile('private-html/login.html',
+    { root: __dirname })
+  );
+
+  app.get('/',
+    connectEnsureLogin.ensureLoggedIn(),
+    (req, res) => res.sendFile('private-html/index.html', {root: __dirname})
+  );
+
+  app.get('/private',
+    connectEnsureLogin.ensureLoggedIn(),
+    (req, res) => res.sendFile('private-html/private.html', {root: __dirname})
+  );
+
+  app.get('/user',
+    connectEnsureLogin.ensureLoggedIn(),
+    (req, res) => res.send({user: req.user})
+  );
+
   app.post('/messages', (req, res) => {
     const message = new Message(req.body);
     message.save((err) =>{
