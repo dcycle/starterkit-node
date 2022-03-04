26904b0806b964002f758b346e5a3146392a6c50
Albert Albala <albert@mediatribe.net>
2022-03-04 11:11:29 -0500

    Please squash this commit in the pull request

--- app/code/webserver.js
+++ app/code/webserver.js
@@ -7,14 +7,20 @@
   'use strict';
 
   module.exports = {
+
+    /** Initialize the webserver. */
     init: function() {
       const express = require('express');
       this.privateApp = express();
       this.privateHttp = require('http').Server(this.privateApp);
     },
+
+    /** Get the app instance. */
     app: function() {
       return this.privateApp;
     },
+
+    /** Get the http server instance. */
     http: function() {
       return this.privateHttp;
     },
