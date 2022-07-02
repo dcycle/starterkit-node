7b64826f99f1dbd4899735409ef0f5366bb16c68
Albert Albala <albert@mediatribe.net>
2022-07-02 09:27:22 -0400

    Please squash this commit in the pull request

--- app/code/server.js
+++ app/code/server.js
@@ -9,7 +9,14 @@
   console.log('* STARTING APPLICATION *');
   console.log('************************');
   const app = require('./app.js');
-  app.init().then(() => {
+  app.init({
+    'modules': {
+      './chat/index.js': {},
+      './authentication/index.js': {},
+      './repl/index.js': {},
+      './express/index.js': {},
+    },
+  }).then(() => {
     app.run(8080, '/usr/src/app/static');
   });
 }());
