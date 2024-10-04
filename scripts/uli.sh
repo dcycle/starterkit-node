fae8ea9853faa774fba3bb11207f7abfc5090f93
SKN <skn@SKNs-MacBook-Pro.local>
2024-10-04 12:57:30 +0530

    new version

--- scripts/uli.sh
+++ scripts/uli.sh
@@ -2,13 +2,13 @@
 #
 # Displays a Unique Log-In link.
 #
-set -e
+# set -e
 
-DOMAIN_AND_PORT=$(docker-compose port node 8428)
+# DOMAIN_AND_PORT=$(docker-compose port node 8428)
 
-./scripts/reset-password.sh admin
+# ./scripts/reset-password.sh admin
 
-echo "=>"
-echo "=> Your node app is at: http://$DOMAIN_AND_PORT"
-echo "=> Log in with the username and password above."
-echo "=>"
+# echo "=>"
+# echo "=> Your node app is at: http://$DOMAIN_AND_PORT"
+# echo "=> Log in with the username and password above."
+# echo "=>"
