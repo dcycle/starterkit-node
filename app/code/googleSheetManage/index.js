5df10ab1fcb79dd7e845e17764e34c6730e0d4b0
SKN <skn@SKNs-MacBook-Pro.local>
2024-11-17 20:49:09 +0530

    code refactored

--- app/code/googleSheetManage/index.js
+++ app/code/googleSheetManage/index.js
@@ -1,14 +1,25 @@
 /**
- * Manage Google Sheets (public or private).
+ * Manage Google Sheets.
+ *
+ * Insert Rows or Column, update data in google sheet.
+ *
+ * User must have edit permission to insert update data particular google sheeet.
  */
 class GoogleSheetManage extends require('../component/index.js') {
 
-  async authenticate() {
-    // Load the Google APIs client library      
+  /**
+   * Authenticate Google Services to get particular sheet.
+   *
+   * @param {string} serviceAccountFile - Service account file path to authenticate google cloud.
+   *
+   * @returns {Promise<string>} - Returns google sheet.
+   */
+  async authenticate(serviceAccountFile) {
+    // Load the Google APIs client library.
     // @ts-expect-error
     const { google } = require('googleapis');
     const auth = new google.auth.GoogleAuth({
-      keyFile: '/output/sensitive-data/celtic-fact-438015-c0-3e39679e41cc.json',
+      keyFile: serviceAccountFile,
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
 
@@ -18,27 +29,100 @@ class GoogleSheetManage extends require('../component/index.js') {
     return sheets;
   }
 
-  // Update a cell in the Google Sheet
-  async updateCell() {
-    const sheets = await this.authenticate();
-    const RANGE = 'Sheet1!A1';  // The range you want to update
-    const SPREADSHEET_ID = '108aKEJNfy5UGdBYZydE8we5hOtUMuombuZyp0CdQLso';
-    const request = {
-      spreadsheetId: SPREADSHEET_ID,
-      range: RANGE,
-      valueInputOption: 'RAW',  // or 'USER_ENTERED' depending on your use case
-      resource: {
-        values: [
-          ['Updated Value'],  // The new value you want to set
-        ],
-      },
-    };
-
+  /**
+   * Update a cell in the Google Sheet.
+   *
+   * @param {string} serviceAccountFile - Service account file path.
+   * @param {string} spreadsheetId - The ID of the Google Spreadsheet.
+   * @param {string} range - Sheet and the range you want to update . ex:- 'Sheet1!A1'
+   * @param {string} values - Values to update. values which is an array of rows,
+   * where each row is an array of values.
+   *
+   * @returns {Promise<bool>} - Result indicating true or false.
+   */
+  async updateCell(serviceAccountFile, spreadsheetId, range, values) {
     try {
+      const sheets = await this.authenticate(serviceAccountFile);
+      const request = {
+        spreadsheetId: spreadsheetId,
+        range: range,
+        // RAW: The values are inserted as-is.
+        // USER_ENTERED: Google Sheets will treat the values as if they were
+        // entered by a user. If you provide a number, it will be treated as a number.
+        // If you provide a string with a formula (like =SUM(A1:A10)), it will be interpreted
+        // as a formula.
+        valueInputOption: 'RAW',
+        resource: {
+          values: [
+            // The new value you want to set.
+            values,
+          ],
+        },
+      };
       const response = await sheets.spreadsheets.values.update(request);
-      console.log('Cell updated:', response.data);
+      if (response.data) {
+        return true;
+      }
+      return false;
     } catch (error) {
       console.error('Error updating cell:', error);
+    }
+  }
+
+  /**
+   * Inserting rows
+   *
+   * @param {string} serviceAccountFile - Service account file path.
+   * @param {string} spreadsheetId - The ID of the Google Spreadsheet.
+   * @param {string} sheetId - The Sheet Id which needs to be updated.
+   * @param {string} dimension - insert 'ROWS' or 'COLUMNS'
+   * @param {string} afterOrBefore - insert 'After' Or 'Before'
+   * @param {int} startIndex - Starting Index.
+   * @param {int} numOfRowColumn - Number of Rows or Columns to insert.
+   *
+   * @returns {Promise<bool>} - Result indicating true or false.
+   */
+  async insertRowsColumns(
+    serviceAccountFile,
+    spreadsheetId,
+    sheetId,
+    dimension,
+    afterOrBefore,
+    startIndex = 'After',
+    numOfRowsColumns) {
+    try {
+      const sheets = await this.authenticate(serviceAccountFile);
+      if (afterOrBefore == 'Before') {
+        // Row before which to insert (Row 9 means starting row 8 in 0-based index)
+        startIndex = startIndex - 1;
+      }
+      let endIndex = startIndex + numOfRowsColumns;
+      const request = {
+        spreadsheetId: spreadsheetId,
+        resource: {
+          requests: [
+            {
+              insertDimension: {
+                range: {
+                  sheetId: sheetId,
+                  dimension: dimension,
+                  startIndex: startIndex,
+                  endIndex: endIndex,
+                },
+                // Whether to inherit formatting, set to true if needed
+                inheritFromBefore: false,
+              },
+            },
+          ],
+        },
+      };
+      const response = await sheets.spreadsheets.batchUpdate(request);
+      if (response.data) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error('Error inserting rows:', error);
     }
   }
 
