0711eaed44dfadae27a479774621c0c4c71db598
SKN <skn@SKNs-MacBook-Pro.local>
2024-11-25 18:02:46 +0530

    observer example

---
+++ app/code/exampleWhatsAppReaction/index.js
@@ -0,0 +1,40 @@
+// @ts-check
+// The TypeScript engine will check all JavaScript in this file.
+
+/**
+ *  ExampleWhatsAppReaction is an example to demonstrate observer functionality.
+ */
+ class ExampleWhatsAppReaction extends require('../component/index.js') {
+
+  /**
+   * Returns the dependencies.
+   * @returns {String[]}
+   */
+   dependencies() {
+    return [
+      // Dependency on express module
+      './express/index.js',
+      './database/index.js',
+      './bodyParser/index.js',
+      './env/index.js'
+    ];
+  }
+
+  async run(app) {
+    app.c('express').addObserver({
+      "module": "webhookWhatsApp",
+      "verb": "receiveMessage",
+      // * for all phone numbers, or else specify a phone number.
+      "applyTo": "+919632324012,+15146779578",
+      "callback": "processReceivedMessage",
+      // or never, or a date...
+      "expire": "+1 hour",
+    });
+
+    // Return the instance of the class
+    return this;
+  }
+
+}
+
+module.exports = new ExampleWhatsAppReaction();
