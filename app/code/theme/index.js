02d7a9a4080f22757a462071db98f6fe3a61e600
root <suresh@vmi601282.junkwebhosting.net>
2025-04-07 18:35:54 +0200

    theme switching

--- app/code/theme/index.js
+++ app/code/theme/index.js
@@ -6,12 +6,17 @@
  class Theme extends require('../component/index.js') {
   async init(app)  {
     super.init(app);
+    // load theme specified in the configuration
+    let theme = this._app.config().modules['./theme/index.js'].default;
+    if (!theme) {
+      theme = "defaultTheme";
+    }
     const expressApp = app.component('./express/index.js').expressApp();
     // @ts-expect-error
     const ejsLayouts = require('express-ejs-layouts');
     // Tell Express to use ejs-layouts
     expressApp.use(ejsLayouts);
-    expressApp.set('layout', 'themes/defaultTheme/layouts/layout');
+    expressApp.set('layout', 'themes/' + theme + '/layouts/layout');
 
     // Create middleware to pass common data to all views (header and footer data)
     expressApp.use((req, res, next) => {
